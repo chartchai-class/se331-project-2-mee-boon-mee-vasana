85f216cf101eaf67d0af38bb541b78e78b2c6145
Wasan Renumat <wasanrenumat@Wasans-MacBook-Pro-5.local>
2024-10-16 15:17:29 +0700

    set front-end and backend (monorepo)

---
+++ apps/project-2-mee-boon-mee-vasana/src/main/java/se331/project2meeboonmeevasana/Project2MeeBoonMeeVasanaApplication.java
@@ -0,0 +1,13 @@
+package se331.project2meeboonmeevasana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2MeeBoonMeeVasanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2MeeBoonMeeVasanaApplication.class, args);
+	}
+
+}
---
+++ apps/project-2-mee-boon-mee-vasana/src/test/java/se331/project2meeboonmeevasana/Project2MeeBoonMeeVasanaApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.project2meeboonmeevasana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2MeeBoonMeeVasanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
